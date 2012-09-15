993fb2f183ec811ee47b52046c307cd4d73a5d86
Marius Høy <marius.hoy@gmail.com>
2012-09-15 16:01:37 +0200

    Start htmlunit. Får det ikke helt til å funke med dagbladet. DEt virker på andre sider

---
+++ src/test/java/com/marius/dagenstegneserie/DagbladetScraperTest.java
@@ -0,0 +1,19 @@
+package com.marius.dagenstegneserie;
+
+import junit.framework.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.IOException;
+
+import static junit.framework.Assert.assertNotNull;
+
+public class DagbladetScraperTest {
+
+    @Test
+    public void testScrapping() throws IOException {
+        DagbladetScraper scraper = new DagbladetScraper();
+        String url = scraper.getImageUrl();
+        assertNotNull(url);
+    }
+}
