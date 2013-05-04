7475fbdf366636e96df2748d7daa1bc4c235a5a2
Marius Høy <marius.hoy@gmail.com>
2013-05-04 20:45:44 +0200

    Kollektivet

--- src/main/java/com/marius/dagenstegneserie/Cartoon.java
+++ src/main/java/com/marius/dagenstegneserie/Cartoon.java
@@ -16,6 +16,8 @@ public enum Cartoon {
     hjalmar("hjalmar", "http://heltnormalt.no/img/hjalmar/$year/$month/$day.jpg", new ReplaceDateCartoonProvider(), false),
     wumo("wumo", "http://heltnormalt.no/img/wumo/$year/$month/$day.jpg", new ReplaceDateCartoonProvider(), false),
     reform94("reform94", "http://heltnormalt.no/img/reform94/$year/$month/$day.jpg", new ReplaceDateCartoonProvider(), false),
+    kollektivet("kollektivet", "http://heltnormalt.no/img/kollektivet/$year/$month/$day.jpg", new ReplaceDateCartoonProvider(), false),
+
     sinfest("sinfest", "http://www.sinfest.net/comikaze/comics/$year-$month-$day.gif", new ReplaceDateCartoonProvider(), false);
 
     private Cartoon(String appUrl, String externalUrl, CartoonProvider provider, boolean store) {
